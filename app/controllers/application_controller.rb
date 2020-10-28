da7b47da030ede2df33b3a9ce0e3c561e6a0800b
Savannah Ipsen <suhvana@gmail.com>
2020-10-27 19:31:22 -0600

    add static welcome blog for brookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(User)
+      root_path
+    else
+      super
+    end
+  end
 end
