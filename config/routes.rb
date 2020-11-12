70edcc380319b9346c1aa578ab9c9c90704a6e87
Savannah Ipsen <suhvana@gmail.com>
2020-11-12 15:38:37 -0700

    add root path to beginning of routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
+  root "pages#home"
   devise_for :users
   resources :posts
   resources :users
-  root "pages#home"
   # get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
